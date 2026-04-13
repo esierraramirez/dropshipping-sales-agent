c0f54fef147b2e956f1de95f6028be315326aaad
esierraramirez <essierra62@ucatolica.edu.co>
2026-04-12 21:18:15 -0500

    Cambio de clase session para la sesion de base de datos

--- backed/app/infrastructure/db/session.py
+++ backed/app/infrastructure/db/session.py
@@ -1,12 +1,24 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
 
 class Base(DeclarativeBase):
     pass
+
 
 def get_db():
     db = SessionLocal()
